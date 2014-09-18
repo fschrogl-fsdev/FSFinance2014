6200dcc975dfddd5beded4d7b2aa65146e5ea58e
Fritz Schrogl <fsdev+brz@schrogl.at>
2014-09-18 17:03:45 +0200

    Fixed some JSF init errors

--- fsfinance-gui-jsf/src/main/java/at/schrogl/fsfinance/webapp/gui/handler/LoginHandler.java
+++ fsfinance-gui-jsf/src/main/java/at/schrogl/fsfinance/webapp/gui/handler/LoginHandler.java
@@ -20,7 +20,7 @@ public class LoginHandler implements Serializable {
 	// ==============================================================
 
 	public void doLogin(ActionEvent event) {
-
+		System.out.println(event.getComponent().getClientId());
 	}
 
 	// ==============================================================
