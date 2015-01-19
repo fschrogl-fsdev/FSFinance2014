593224c9158b08fa0549749ff7b1e6cec927952c
Fritz Schrogl <fsdev+brz@schrogl.at>
2015-01-19 17:29:15 +0100

    UI: Settings page (WIP, Ajax)

--- fsfinance-gui-jsf/src/main/java/at/schrogl/fsfinance/gui/handler/SettingsHandler.java
+++ fsfinance-gui-jsf/src/main/java/at/schrogl/fsfinance/gui/handler/SettingsHandler.java
@@ -17,8 +17,6 @@
 package at.schrogl.fsfinance.gui.handler;
 
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
@@ -37,12 +35,24 @@ public class SettingsHandler implements Serializable {
 	private static final Logger LOGGER = LoggerFactory.getLogger(SettingsHandler.class);
 
 	private static final String[] tabnames = { "Profile", "Accounts", "Labels", "Delete Profile" };
+	
+	private String activeTab = null; 
 
 	@ManagedProperty("#{applicationConfig}")
 	private ApplicationConfig appConfig;
 	
-	public List<String> getTabnames() {
-		return Arrays.asList(tabnames);
+	public String[] getTabnames() {
+		return tabnames;
+	}
+	
+	public String setActiveTab(String activeTab) {
+		this.activeTab = activeTab;
+		LOGGER.debug(activeTab);
+		return null;
+	}
+	
+	public String getActiveTab() {
+		return activeTab;
 	}
 
 	// ==============================================================
